fe7ec2bcc10b1aa0cc405d35ef612409ee9517da
Justin MacGregor <justinmacgregor4@gmail.com>
2020-05-16 22:26:55 -0400

    Associated NavigationView buttons with different pages

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -17,14 +17,42 @@ using Windows.UI.Xaml.Navigation;
 
 namespace Blanket_Launcher
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked) {
+                ContentFrame.Navigate(typeof(SettingsPage));
+            }
+
+            else {
+                // find NavigationViewItem with Content that equals InvokedItem
+                var item = sender.MenuItems.OfType<NavigationViewItem>().First(x => (string)x.Content == (string)args.InvokedItem);
+                NavView_Navigate(item as NavigationViewItem);
+            }
+        }
+
+        private void NavView_Navigate(NavigationViewItem item)
+        {
+            switch (item.Tag) {
+                case "store":
+                    ContentFrame.Navigate(typeof(StorePage));
+                    break;
+
+                case "games":
+                    ContentFrame.Navigate(typeof(GamesPage));
+                    break;
+
+                case "launchers":
+                    ContentFrame.Navigate(typeof(LaunchersPage));
+                    break;
+            }
         }
     }
 }
