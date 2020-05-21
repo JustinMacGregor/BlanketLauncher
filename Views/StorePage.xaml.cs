6e9e2e68797b7362fff637f68ae766e2e8f8d4ff
Justin MacGregor <justinmacgregor4@gmail.com>
2020-05-21 13:43:09 -0400

    Fixed back button bugs

--- Views/StorePage.xaml.cs
+++ Views/StorePage.xaml.cs
@@ -1,10 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Diagnostics;
+using Windows.UI.Xaml.Controls;
 
 namespace Blanket_Launcher
 {
     public sealed partial class StorePage : Page {
 
-        string _currentHost = "";
+        public int _navigation = -1;
 
         public StorePage() {
             this.InitializeComponent();
@@ -15,6 +16,7 @@ namespace Blanket_Launcher
                 {
                     case "back":
                         if (webView.CanGoBack) {
+                            _navigation = _navigation - 2;
                             webView.GoBack();
                         }
                         break;
@@ -30,10 +32,36 @@ namespace Blanket_Launcher
                         break;
 
                     default:
-                    //to fix back button bug (selecting it multiple times will bring to other store page), change the cursor if the domain changes
-                        webView.Navigate(new System.Uri(args.InvokedItemContainer.Tag.ToString()));
+                    _navigation = -2;
+                    // TO-DO fix back button bug (selecting it multiple times will bring to other store page), change the cursor if the domain changes
+                    webView.Navigate(new System.Uri(args.InvokedItemContainer.Tag.ToString()));
                         break;
                 }
         }
+
+        private void DOMContentLoaded(WebView sender, WebViewDOMContentLoadedEventArgs args)
+        {
+            BackButton.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            _navigation++;
+
+            if (_navigation < -1) { _navigation = -1; }
+
+            System.Diagnostics.Debug.WriteLine(_navigation.ToString());
+            if (_navigation == -1) { BackButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed; } 
+            else { BackButton.Visibility = Windows.UI.Xaml.Visibility.Visible; }
+        }
+
+        private void ContentLoading(WebView sender, WebViewContentLoadingEventArgs args)
+        {
+            BackButton.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private void NewWindowRequested(WebView sender, WebViewNewWindowRequestedEventArgs args)
+        {
+            webView.Navigate(args.Uri);
+            args.Handled = true;
+        }
+
+        // TO-DO: Save URL for each tab (session only)
     }
 }
